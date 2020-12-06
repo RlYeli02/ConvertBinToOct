b07bf302950b245bf0c98a038bf2e49bbb243207
Franyeliza <franyelizarodriguez@live.com>
2020-12-06 13:11:44 -0400

    test 2 passed

--- convert_test.py
+++ convert_test.py
@@ -7,6 +7,10 @@ class TestBinToOct(unittest.TestCase):
         result = convert.BinToOct("011.011")
         self.assertEqual(result, "3.3")
 
+    def test_convert_to_binary_with_one_decimal(self):
+        result = convert.BinToOct("0110011")
+        self.assertEqual(result, "063")
+
 
 if __name__ == '__main__':
     unittest.main()
